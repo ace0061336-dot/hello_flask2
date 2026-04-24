a23481f9209cecfa3eed47e83273a3e3d1fb2f16
ace0061336-dot <ace0061336@gmail.com>
2026-04-24 09:46:21 +0900

    HTML 파일 보내기

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return{
-        "name": "홍길동",
-        "role": "ㄱㄴ소로ㅓ",
-        "status":"ㅗ허ㅓ옥ㄳ",
-        "skills": ["Ubuntu","VS Code", "Python", "Flask"]
-    }
+    # 이제는 JSON이 아니라 HTML 파일을 보냅니다
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
