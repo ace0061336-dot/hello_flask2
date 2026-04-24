aee728fb89069dbeed3887645700f9adb70b9bf8
ace0061336-dot <ace0061336@gmail.com>
2026-04-24 10:52:39 +0900

    260414 form 전송 구조

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request,redirect
 
 app = Flask(__name__)
+messages = []
 
 @app.route('/')
 def home():
@@ -14,7 +15,7 @@ def home():
         "Bash", "Shell",
         "Dart", "Swift"
     ]
-    return render_template('index.html', data=skills)
+    return render_template('index.html', skills=skills, messages=messages)
 
 
 @app.route('/send', methods=['POST'])
@@ -23,7 +24,10 @@ def send():
     level = request.form.get('level')
     status = request.form.get('status')
 
-    return skill + " : " + level + " : " + status
+    messages.append(skill + " / " + level + " / " + status)
+
+    return redirect('/')
+
 
 
 if __name__ == '__main__':
